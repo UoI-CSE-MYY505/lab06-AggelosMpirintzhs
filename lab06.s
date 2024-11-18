a0c79b1c4f381aa3b44aa6c1b6088884bb1fc340
Aggelos Mpirintzhs <92552419+AggelosMpirintzhs@users.noreply.github.com>
2024-11-18 23:50:57 +0200

    Update lab06.s

--- lab06.s
+++ lab06.s
@@ -1,11 +1,11 @@
 
 .data
 data: .word 1
-
 .text
     la   x10, data
     li   x11, 0
     li   x9,  -1
+
 
 loop:
 l1: addi x10, x10, 4 
@@ -16,6 +16,7 @@ l5: add  x5,  x5, x5
 l6: sw   x5,  0(x10) 
 l7: j    loop 
 out: 
+
 l8: addi x17, x0, 10
 l9: ecall
 
